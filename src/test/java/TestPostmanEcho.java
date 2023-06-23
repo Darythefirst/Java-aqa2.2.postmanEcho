732003df8eade0fd31c2a275e5347caad1e8c4ec
Dary <Dafna_pelmeshka@mail.ru>
2023-06-23 22:33:35 +0300

    Initial commit

---
+++ src/test/java/TestPostmanEcho.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class TestPostmanEcho {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data",equalTo("Hello"))
+        ;
+    }
+}
